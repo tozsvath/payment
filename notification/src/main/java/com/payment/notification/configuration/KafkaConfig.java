0f6c30b505ebe40606e740d208e588cd86a6d9a2
tozsvath <tamas.ozsvath17@gmail.com>
2025-04-01 09:06:20 +0200

    simplify kafka config

--- notification/src/main/java/com/payment/notification/configuration/KafkaConfig.java
+++ notification/src/main/java/com/payment/notification/configuration/KafkaConfig.java
@@ -24,27 +24,4 @@ public class KafkaConfig {
     return new MappingJackson2MessageConverter();
   }
 
-  @Bean
-  public ConsumerFactory<String, String> consumerFactory() {
-    Map<String, Object> props = new HashMap<>();
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap_severs);
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-group");
-    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-        "org.apache.kafka.common.serialization.StringDeserializer");
-    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-        "org.apache.kafka.common.serialization.StringDeserializer");
-    return new DefaultKafkaConsumerFactory<>(props);
-  }
-
-  @Bean
-  public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-    ConcurrentKafkaListenerContainerFactory<String, String> factory =
-        new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory());
-
-    factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-
-    return factory;
-  }
-
 }
